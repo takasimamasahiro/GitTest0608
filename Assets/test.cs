b179216c9752384f81530205c32d1a4cdde89f8d
takasimamasahiro <134473651+takasimamasahiro@users.noreply.github.com>
2024-06-08 19:29:52 +0900

    test

--- Assets/test.cs
+++ Assets/test.cs
@@ -1,10 +1,12 @@
+using UnityEngine;
+
 public class test : MonoBehaviour
 {
-    Vector3 initPos = new Vector3(0, 10, 0);
+    Vector3 initPos = new Vector3(1, 2, 3);
     // Start is called before the first frame update
     void Start()
     {
-
+        this.transform.position = initPos;
     }
 
     // Update is called once per frame
