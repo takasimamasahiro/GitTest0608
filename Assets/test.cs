9e52cce7fa6db46dc41189bd5dcef92a65a4577d
takasimamasahiro <134473651+takasimamasahiro@users.noreply.github.com>
2024-06-08 19:17:30 +0900

    test

--- Assets/test.cs
+++ Assets/test.cs
@@ -1,20 +1,15 @@
-using UnityEngine;
-
 public class test : MonoBehaviour
 {
-    Vector3 initPos = new Vector3(0, 0, 0);
+    Vector3 initPos = new Vector3(0, 10, 0);
     // Start is called before the first frame update
     void Start()
     {
-        this.transform.position = initPos;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            this.transform.position += new Vector3(0.1f, 0, 0);
-        }
+        this.transform.position += new Vector3(0.1f, 0, 0);
     }
 }
